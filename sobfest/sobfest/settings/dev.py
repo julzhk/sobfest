ee60acd9592ea7f411b9396ad7f3a60976a9c34f
julian harley <julian.harley.fr@harpercollins.co.uk>
2019-11-14 11:32:53 +0000

    init commit

---
+++ sobfest/sobfest/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'zc59=a%ahi5x-+8)e290&-e*&co8cy*3d#ye)7am6^$w_)elnt'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
