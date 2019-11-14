3e090dfe596e67f805f23904740f218a1139b092
julian harley <julian.harley.fr@harpercollins.co.uk>
2019-11-14 21:30:51 +0000

    add heroku production settings

--- sobfest/sobfest/settings/production.py
+++ sobfest/sobfest/settings/production.py
@@ -1,3 +1,5 @@
+import dj_database_url
+import django_heroku
 from .base import *
 
 DEBUG = False
@@ -6,3 +8,10 @@ try:
     from .local import *
 except ImportError:
     pass
+
+DATABASES['default'] = dj_database_url.config()
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
