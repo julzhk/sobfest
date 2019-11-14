43fa11c2438f782bd48ab344843c7ebd7e9e5140
julian harley <julian.harley.fr@harpercollins.co.uk>
2019-11-14 22:58:39 +0000

    fix settings

--- sobfest/sobfest/settings/production.py
+++ sobfest/sobfest/settings/production.py
@@ -1,15 +1,24 @@
 import dj_database_url
 import django_heroku
 from .base import *
+import os
 
-DEBUG = False
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DEBUG = True
 
 try:
     from .local import *
 except ImportError:
     pass
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-DATABASES['default'] = dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -19,3 +28,4 @@ ALLOWED_HOSTS = ['*']
 import django_heroku
 django_heroku.settings(locals())
 
+SECRET_KEY='3245678i76543567865'
